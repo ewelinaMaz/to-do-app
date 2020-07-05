eb8c65643a305b401eee1bfcedd04fad31c8038c
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2020-07-05 13:20:23 +0200

    add init files and socket.io

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+const socket = require("socket.io");
+
+const app = express();
+
+const server = app.listen(8000, () => {
+    console.log("Server is running on Port:", 8000);
+  });
+
+  const io = socket(server);
